9d1bcabff050b94ffcd3eca6a6dd3c1691d9f20d
Nina-08 <160328772+Nina-08@users.noreply.github.com>
2024-02-23 21:23:19 -0600

    Listas

--- Listas/src/main/java/com/mycompany/listas/Nodo.java
+++ Listas/src/main/java/com/mycompany/listas/Nodo.java
@@ -2,12 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.main;
+package com.mycompany.listas;
 
-/**
- *
- * @author Daphne López
- */
+
 public class Nodo {
-    
+
+	int dato;
+	
+	Nodo siguiente;
+	
+	
+	public Nodo(int dato) {
+		this.dato = dato;
+		this.siguiente = null;
+	}
+	
 }
